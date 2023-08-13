4152a149d341d22640880705c79c2da4da6ba639
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 00:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const parseCode = (str) => {
   // your code here
+	const [firstName, lastName, id] = str.split('000');
+
+  return {
+  firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
 };
 
 // Do not change the code below
 const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+alert(JSON.stringfy.(parseCode(str)));
